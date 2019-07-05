333800b4d1b77a7cd38239763f00cfa8aa31f3ff
emysaulintec <esoto@oriontek.do>
2019-07-05 11:44:08 -0400

    init project

---
+++ Project/UniversalTranslator/UniversalTranslator/Converter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalTranslator
+{
+
+
+    public class Converter
+    {
+
+        public static Dictionary<string, double> allowedConvertion = new Dictionary<string, double>() {
+            { "km", 3}, // Kilometro
+            { "hm", 2 }, // Hectometro
+            { "dam", 1 }, // Decametro
+            {"m", 0 }, // Metro
+            {"dm", -1 }, // Decimetro
+            {"cm", -2 }, // Centimetro
+            {"mm", -3 }  // milimetro
+        };
+
+
+        // 
+        /// <summary>
+        /// This alghoritm was created based in this url https://www.youtube.com/watch?v=ZzqP8K2NGKk&t=98s
+        /// </summary>
+        /// <param name="origin"></param>
+        /// <param name="destiny"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static double GetConvertion(string origin, string destiny, double value)
+        {
+            double distance = Math.Abs(allowedConvertion[origin] - allowedConvertion[destiny]);
+            if (distance == 0)
+                return value;
+
+            if (allowedConvertion[origin] >= allowedConvertion[destiny])
+                return value * Math.Pow(10, distance);
+            else
+                return value / Math.Pow(10, distance);
+        }
+    }
+}
---
+++ Project/UniversalTranslator/UniversalTranslator/FileController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalTranslator
+{
+    public class FileController
+    {
+        public void LoadFileAndConvert(string path)
+        {
+            var result = LoadFile(path);
+            if (result.Count() > 0)
+                WriteResult(result, path);
+        }
+
+
+        public IEnumerable<Entry> LoadFile(string path)
+        {
+            string[] lines = LoadFilePath(path);
+
+            // line = "value,origin,destiny"
+            foreach (var line in lines)
+            {
+                string[] splitedLine = line.Split(',');
+                if (!ValidateLine(splitedLine))
+                {
+                    Console.WriteLine("Linea no valida");
+                    continue;
+                }
+
+                double value = double.Parse(splitedLine[0]);
+                string origin = splitedLine[1];
+                string destinty = splitedLine[2];
+
+                yield return new Entry()
+                {
+                    Value = value,
+                    Origin = origin,
+                    Destiny = destinty,
+                };
+            }
+
+        }
+
+
+        private string[] LoadFilePath(string path)
+        {
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Esta direccion no existe");
+                return Array.Empty<string>();
+            }
+
+            return File.ReadAllLines(path);
+        }
+        // line = "value,origin,destiny"
+        private bool ValidateLine(string[] splitedLine)
+        {
+            if (splitedLine.Length != 3)
+            {
+                Console.WriteLine("El formato valido es value,origin,destiny. Usted no cumple con este formato");
+                return false;
+            }
+
+            if (!IsNumeric(splitedLine[0]))
+            {
+                Console.WriteLine("El valor a convertir debe ser numerico.");
+                return false;
+            }
+
+            if (!(Converter.allowedConvertion.Keys.Contains(splitedLine[1]) && Converter.allowedConvertion.Keys.Contains(splitedLine[2])))
+            {
+                Console.WriteLine("conversion de " + splitedLine[1] + " a " + splitedLine[2] + " no permitida");
+                return false;
+            }
+
+            return true;
+        }
+
+
+        private bool IsNumeric(string v)
+        {
+            if (v == "0")
+                return true;
+
+            decimal.TryParse(v, out decimal value);
+            return value != 0;
+        }
+
+
+        private void WriteResult(IEnumerable<Entry> result, string path)
+        {
+            var r = result.Select(e => { return $"{e.Value.ToString()},{e.Origin.ToString()},{e.Destiny.ToString()}, {Converter.GetConvertion(e.Origin.ToString(), e.Destiny.ToString(), e.Value)}"; }).ToArray();
+
+
+            var originPath = System.IO.Path.GetDirectoryName(path);
+
+            string pathOutput = originPath + $"\\result_{DateTime.Now.Hour}_{DateTime.Now.Minute}__{DateTime.Now.Second}_{DateTime.Now.Millisecond}.txt";
+            File.WriteAllLines(pathOutput, r);
+
+            var directory = Path.GetDirectoryName(pathOutput);
+
+            Process.Start(directory);
+
+        }
+
+
+
+
+    }
+}
